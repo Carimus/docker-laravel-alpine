cb3641a0d2d04c4cb609e67ca0ebf9e31ffaa104
Brandon Phillips <bericp1@gmail.com>
2019-01-02 19:55:33 -0700

    update image publishing to tag with git short commit and only if local git repo is clean

--- publish.sh
+++ publish.sh
@@ -2,18 +2,41 @@
 
 set -e
 
-echo "Building image for php7.2-cli (default image):"
+GIT_STATUS="$(git status -s)"
+GIT_SHORT_COMMIT="$(git rev-parse --short HEAD)"
 
-docker build -t carimus/laravel-alpine:php7.2-cli \
+REPO_NAME="carimus/laravel-alpine"
+CLI_TAG="php7.2-cli"
+FPM_TAG="php7.2-fpm"
+
+if [[ -n "$GIT_STATUS" ]]; then
+  echo "There are untracked changes or the working tree is dirty."
+  echo "The image is tagged with the short git commit so please commit any changes before publishing."
+  exit 1
+fi
+
+echo "Will build images with the following tags:"
+echo -e "Based on php:7.2-cli-alpine:\t$CLI_TAG, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$CLI_TAG"
+echo -e "Based on php:7.2-fpm-alpine:\t$FPM_TAG, $GIT_SHORT_COMMIT-$FPM_TAG"
+echo
+
+echo "Building image based on php:7.2-cli-alpine (default image):"
+
+docker build \
+  -t carimus/laravel-alpine:$CLI_TAG \
   -t carimus/laravel-alpine:latest \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG \
   -f ./php7.2-cli/Dockerfile \
   .
 
 echo
 
-echo "Building image for php7.2-fpm:"
+echo "Building image based on php:7.2-fpm-alpine:"
 
-docker build -t carimus/laravel-alpine:php7.2-fpm \
+docker build \
+  -t carimus/laravel-alpine:$FPM_TAG \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG \
   -f ./php7.2-fpm/Dockerfile \
   .
 
@@ -21,9 +44,12 @@ echo
 
 echo "Pushing images to docker hub:"
 
-docker push carimus/laravel-alpine:php7.2-cli
+docker push carimus/laravel-alpine:$CLI_TAG
 docker push carimus/laravel-alpine:latest
-docker push carimus/laravel-alpine:php7.2-fpm
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG
+docker push carimus/laravel-alpine:$FPM_TAG
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG
 
 echo
 
