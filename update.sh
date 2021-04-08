72351221822dc525b4416353f65574d288c51f23
Brandon Phillips <bericp1@gmail.com>
2021-04-08 13:21:06 -0600

    fix(*): update to latest phpredis version for 7.4 and 8.0 so they compile

--- update.sh
+++ update.sh
@@ -12,24 +12,30 @@ mkdir -p ./php8.0-fpm/
 
 cp ./Dockerfile.template ./php7.3-cli/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:7.3-cli-alpine/' ./php7.3-cli/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/4.2.0/' ./php7.3-cli/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__//' ./php7.3-cli/Dockerfile
 
 cp ./Dockerfile.template ./php7.3-fpm/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:7.3-fpm-alpine/' ./php7.3-fpm/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/4.2.0/' ./php7.3-fpm/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__/nginx/' ./php7.3-fpm/Dockerfile
 
 cp ./Dockerfile.template ./php7.4-cli/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:7.4-cli-alpine/' ./php7.4-cli/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/5.3.2/' ./php7.4-cli/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__//' ./php7.4-cli/Dockerfile
 
 cp ./Dockerfile.template ./php7.4-fpm/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:7.4-fpm-alpine/' ./php7.4-fpm/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/5.3.2/' ./php7.4-fpm/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__/nginx/' ./php7.4-fpm/Dockerfile
 
 cp ./Dockerfile.template ./php8.0-cli/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:8.0-cli-alpine/' ./php8.0-cli/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/5.3.2/' ./php8.0-cli/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__//' ./php8.0-cli/Dockerfile
 
 cp ./Dockerfile.template ./php8.0-fpm/Dockerfile
 sed -i '' 's/__BASE_IMAGE__/php:8.0-fpm-alpine/' ./php8.0-fpm/Dockerfile
+sed -i '' 's/__PHPREDIS_VERSION__/5.3.2/' ./php8.0-fpm/Dockerfile
 sed -i '' 's/__EXTRA_APK_PACKAGES__/nginx/' ./php8.0-fpm/Dockerfile
