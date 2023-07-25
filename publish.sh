272056f59f2ec31c60bee2c5105da1def1ab7bca
Andrew Gillingham <andrewg@carimus.com>
2023-07-25 14:24:49 -0400

    feat: add PHP81, PHP82 images

--- publish.sh
+++ publish.sh
@@ -14,6 +14,10 @@ CLI_TAG_74="php7.4$CLI_TAG_SUFFIX"
 FPM_TAG_74="php7.4$FPM_TAG_SUFFIX"
 CLI_TAG_80="php8.0$CLI_TAG_SUFFIX"
 FPM_TAG_80="php8.0$FPM_TAG_SUFFIX"
+CLI_TAG_81="php8.1$CLI_TAG_SUFFIX"
+FPM_TAG_81="php8.1$FPM_TAG_SUFFIX"
+CLI_TAG_82="php8.2$CLI_TAG_SUFFIX"
+FPM_TAG_82="php8.2$FPM_TAG_SUFFIX"
 
 if [[ -n "$GIT_STATUS" ]]; then
   echo "There are untracked changes or the working tree is dirty."
@@ -22,7 +26,11 @@ if [[ -n "$GIT_STATUS" ]]; then
 fi
 
 echo "Will build images with the following tags:"
-echo -e "Based on php:8.0-cli-alpine:\t$CLI_TAG_80, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$CLI_TAG_80"
+echo -e "Based on php:8.2-cli-alpine:\t$CLI_TAG_82, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$CLI_TAG_82"
+echo -e "Based on php:8.2-fpm-alpine:\t$FPM_TAG_82, $GIT_SHORT_COMMIT-$FPM_TAG_82"
+echo -e "Based on php:8.1-cli-alpine:\t$CLI_TAG_81, $GIT_SHORT_COMMIT-$CLI_TAG_81"
+echo -e "Based on php:8.1-fpm-alpine:\t$FPM_TAG_81, $GIT_SHORT_COMMIT-$FPM_TAG_81"
+echo -e "Based on php:8.0-cli-alpine:\t$CLI_TAG_80, $GIT_SHORT_COMMIT-$CLI_TAG_80"
 echo -e "Based on php:8.0-fpm-alpine:\t$FPM_TAG_80, $GIT_SHORT_COMMIT-$FPM_TAG_80"
 echo -e "Based on php:7.4-cli-alpine:\t$CLI_TAG_74, $GIT_SHORT_COMMIT-$CLI_TAG_74"
 echo -e "Based on php:7.4-fpm-alpine:\t$FPM_TAG_74, $GIT_SHORT_COMMIT-$FPM_TAG_74"
@@ -30,12 +38,52 @@ echo -e "Based on php:7.3-cli-alpine:\t$CLI_TAG_73, $GIT_SHORT_COMMIT-$CLI_TAG_7
 echo -e "Based on php:7.3-fpm-alpine:\t$FPM_TAG_73, $GIT_SHORT_COMMIT-$FPM_TAG_73"
 echo
 
-echo "Building image based on php:8.0-cli-alpine (default image):"
+echo "Building image based on php:8.2-cli-alpine (default image):"
+
+docker build \
+  -t carimus/laravel-alpine:$CLI_TAG_82 \
+  -t carimus/laravel-alpine:latest \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_82 \
+  -f ./php8.2-cli/Dockerfile \
+  .
+
+echo
+
+echo "Building image based on php:8.2-fpm-alpine:"
+
+docker build \
+  -t carimus/laravel-alpine:$FPM_TAG_82 \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG_82 \
+  -f ./php8.2-fpm/Dockerfile \
+  .
+
+echo
+
+echo "Building image based on php:8.1-cli-alpine:"
+
+docker build \
+  -t carimus/laravel-alpine:$CLI_TAG_81 \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_81 \
+  -f ./php8.1-cli/Dockerfile \
+  .
+
+echo
+
+echo "Building image based on php:8.1-fpm-alpine:"
+
+docker build \
+  -t carimus/laravel-alpine:$FPM_TAG_81 \
+  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG_81 \
+  -f ./php8.1-fpm/Dockerfile \
+  .
+
+echo
+
+echo "Building image based on php:8.0-cli-alpine:"
 
 docker build \
   -t carimus/laravel-alpine:$CLI_TAG_80 \
-  -t carimus/laravel-alpine:latest \
-  -t carimus/laravel-alpine:$GIT_SHORT_COMMIT \
   -t carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_80 \
   -f ./php8.0-cli/Dockerfile \
   .
@@ -94,9 +142,19 @@ echo
 
 echo "Pushing images to docker hub:"
 
-docker push carimus/laravel-alpine:$CLI_TAG_80
+docker push carimus/laravel-alpine:$CLI_TAG_82
 docker push carimus/laravel-alpine:latest
 docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_82
+docker push carimus/laravel-alpine:$FPM_TAG_82
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG_82
+
+docker push carimus/laravel-alpine:$CLI_TAG_81
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_81
+docker push carimus/laravel-alpine:$FPM_TAG_81
+docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG_81
+
+docker push carimus/laravel-alpine:$CLI_TAG_80
 docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$CLI_TAG_80
 docker push carimus/laravel-alpine:$FPM_TAG_80
 docker push carimus/laravel-alpine:$GIT_SHORT_COMMIT-$FPM_TAG_80
